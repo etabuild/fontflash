66b97a554ee6553402d046b992ee2cd42a0419b0
ym174 <ym1743kp@gmail.com>
2023-02-07 21:35:01 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Main from "./components/Main.vue";
+</script>
+<template>
+    <Main/>
+</template>
+
+<style scoped>
+.logo.vite:hover {
+    filter: drop-shadow(0 0 2em #747bff);
+}
+
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #249b73);
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="toolbar"></div>
+    <div id="viewer">
+        <textarea></textarea>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "Main.vue"
+}
+</script>
+
+<style scoped>
+    @import "stylesheets/default.css";
+</style>
