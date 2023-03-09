20a46d8a02b23fd0dce23bf8839d0d90340277c8
ym174 <ym1743kp@gmail.com>
2023-03-09 23:21:35 +0900

    Add font file detection feature in core process

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,18 +9,22 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 */
-use std::env;
+use std::{env, path};
+use std::borrow::Cow;
 use tauri::{Manager};
 use window_shadows::set_shadow;
 use std::fs;
 use std::fs::Metadata;
-use rusttype::{ Font};
+use std::io::BufRead;
+use rusttype::{Font};
+use tauri::api::dir::is_dir;
 
+use serde::{ Serialize, Deserialize };
 
+#[derive(Debug, Serialize, Deserialize)]
 struct FileData {
-    path: String,
-    name: String,
-
+    args: Vec<String>,
+    dir_files: Vec<String>,
 }
 
 
@@ -35,16 +39,52 @@ fn get_args() -> String {
     return paths.join(",");
 }
 
+
 #[tauri::command]
-fn get_data() -> () {
+fn get_data() -> FileData {
     /*    let path: String = env::args().nth(0).unwrap();
     */
     let mut args: Vec<String> = env::args().collect();
-    let path = r"C:\Users\ym174\OneDrive\デスクトップ\TBGoStdR-C6.otf";
+    let mut dir_list: Vec<String> = vec![];
+    let mut path = "";
+    if args.len() == 1 {
+        path = r"C:\Users\ym174\Desktop\TBGoStdR-C6.otf";
+        println!("ok");
+    } else {
+        path = &*args[1];
+    }
+    let filename = path.split(r"\").collect::<Vec<&str>>()[path.split(r"\").collect::<Vec<&str>>().len() - 1];
+    let dirname = path.replace(filename, "");
 
+    println!("{}", dirname.to_string());
+    let target = path::PathBuf::from(dirname);
+
+    for dir_entry in fs::read_dir(target).unwrap() {
+        // dir_entry(Result<DirEntry, Error>型)をfile_path(PathBuf型)に変換する
+        let entry = dir_entry.unwrap();
+        if entry.file_type().unwrap().is_file() {
+            let file_path: String = entry.file_name().into_string().unwrap();
+            /*            let file_path : &str = file_path.unwrap();
+            */            println!("{:?}", file_path);
+            let splited_name = file_path.split(r".").collect::<Vec<&str>>();
+            if splited_name.len() > 1 {
+                println!("{}", splited_name.len());
+                let extension = splited_name[splited_name.len() - 1];
+                if extension == "woff2" ||
+                    extension == "woff" ||
+                    extension == "ttf" ||
+                    extension == "otf" {
+                    dir_list.push(file_path.to_string());
+                }
+            }
+        }
+    }
+    println!("{:?}", dir_list);
+    return FileData {
+        args,
+        dir_files: dir_list,
+    };
 }
-
-
 
 
 /*
@@ -64,10 +104,11 @@ fn main() {
             println!("{}, {argv:?}, {cwd}", app.package_info().name);
             app.emit_all("instance_detection", argv.join(",")).unwrap();
         }))
-        .invoke_handler(tauri::generate_handler![get_args])
+        .invoke_handler(tauri::generate_handler![get_args,get_data])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
 
 /*fn get_metadata ->
 */
